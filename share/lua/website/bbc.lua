942247e6ee3ab3ea47e0cc6b1e137ba30df95b90
Pierre Carrier <prc@redhat.com>
2011-04-14 02:53:36 +0100

    Add support for BBC iPlayer
    Currently untested.
    
    TODO:
    - Test & fix :)
    - Better error message for non-UK residents
    - Support for live streams
    - Write tests? (tricky, needs UK IP, and everything expires AFAIK)
    
    Would require changes in the quvi plugins API:
    - Provide more metadata, including description
      an RDF field could be an overkill, standardized answer
      - Support for subtitles (no fields available)
    - Simultaneous support for http (N95) & rtmp streams
      (might only require lua def of C.protos_ as bitmask already?)
    - Provide the byte length of streams
      (quvi should allow the plugins to provide this themselves,
          and fallback on its C code)
      - More discutavble, support for URLs of lists of episodes?
        (would involve "arrays", with metadata for the series AND
            each item)

---
+++ share/lua/website/bbc.lua
@@ -0,0 +1,182 @@
+
+-- quvi
+-- Copyright (C) 2010  quvi project
+--
+-- This file is part of quvi <http://quvi.googlecode.com/>.
+--
+-- This library is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU Lesser General Public
+-- License as published by the Free Software Foundation; either
+-- version 2.1 of the License, or (at your option) any later version.
+--
+-- This library is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+-- Lesser General Public License for more details.
+--
+-- You should have received a copy of the GNU Lesser General Public
+-- License along with this library; if not, write to the Free Software
+-- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+-- 02110-1301  USA
+
+
+-- Obtained with grep -oP '(?<=service=")[^"]+(?=")' on config
+local fmt_id_lookup = {
+  high     = 'iplayer_streaming_h264_flv_high',
+  standard = 'iplayer_streaming_h264_flv',
+  low      = 'iplayer_streaming_h264_flv_lo',
+  vlow     = 'iplayer_streaming_h264_flv_vlo'
+  -- iplayer_streaming_n95_3g
+  -- iplayer_streaming_n95_wifi
+}
+
+-- Identify the script.
+function ident(self)
+    package.path = self.script_dir .. '/?.lua'
+    local C      = require 'quvi/const'
+    local r      = {}
+    r.domain     = "www.bbc.co.uk"
+    r.formats    = "default|best"
+    for k,_ in pairs(fmt_id_lookup) do
+        r.formats = r.formats .."|".. k
+    end
+    r.categories = C.proto_rtmp
+    local U      = require 'quvi/util'
+    r.handles    = U.handles(self.page_url, {r.domain}, {"/iplayer/"})
+    return r
+end
+
+-- Parse video URL.
+function parse(self)
+
+    function create_uri_for_limelight_level3_iplayertok(params)
+        params.uri = 'rtmp://' .. params.server .. ':1935/ondemand?_fcs_vhost='
+            .. params.server .. '&auth=' .. params.authString .. '&aifp=v001&slist='
+            .. params.identifier
+    end
+
+    function process_akamai(params)
+        params.application = params.application or 'ondemand'
+        params.application = params.application .. '?_fcs_vhost=' .. params.server .. '&undefined'
+        params.uri = 'rtmp://' .. params.server .. ':1935/' .. params.application
+        if not params.authString:find("&aifp=") then
+            params.authString = params.authString .. '&aifp=v001'
+        end
+        if not params.authString:find("&slist=") then
+            params.identifier = params.identifier:gsub('^mp[34]:', '')
+            params.authString = params.authString .. '&slist=' .. params.identifier
+        end
+        params.identifier = params.identifier .. '?' .. params.authString
+        params.uri = params.uri .. '&' .. params.authString
+        params.application = params.application .. '&' .. params.authString
+        params.tcurl = 'rtmp://' .. params.server .. ':80/' .. params.application
+    end
+
+    function process_limelight_level3(params)
+        create_uri_for_limelight_level3_iplayertok(params)
+        params.application = params.application .. '&' .. params.authString
+        params.tcurl = 'rtmp://' .. params.server .. ':1935/' .. params.application
+    end
+
+    function process_iplayertok(params)
+        create_uri_for_limelight_level3_iplayertok(params)
+        params.identifier = params.identifier .. '?' .. params.authString
+        params.identifier = params.identifier:gsub('^mp[34]:', '')
+        params.tcurl = 'rtmp://' .. params.server .. ':1935/' .. params.application
+    end
+
+    self.host_id = 'bbc'
+
+    local _,_,s = self.page_url:find('episode/(.-)/')
+    local episode_id = s or error('no match: episode id')
+    self.id = episode_id
+
+    local playlist_uri =
+        'http://www.bbc.co.uk/iplayer/playlist/' .. episode_id
+    local playlist = quvi.fetch(playlist_uri, {fetch_type = 'playlist'})
+
+    local pl_item_p,_,s = playlist:find('<item kind="programme".-identifier="(.-)"')
+    local media_id = s or error('no match: media id')
+
+    local _,_,s = playlist:find('duration="(%d+)"', pl_item_p)
+    self.duration = tonumber(s) or 0
+
+    local _,_,s = playlist:find('<title>(.-)</title>')
+    self.title = s or error('no match: video title')
+
+    local _,_,s = playlist:find('<link rel="holding" href="(.-)"')
+    self.media_thumbnail_url = s or ""
+
+    -- stolen from http://lua-users.org/wiki/MathLibraryTutorial
+    math.randomseed(os.time()) math.random() math.random() math.random()
+    local config_uri =
+        'http://www.bbc.co.uk/mediaselector/4/mtis/stream/' ..
+        media_id .. "?cb=" .. math.random(10000)
+
+    local config = quvi.fetch(config_uri, {fetch_type = 'config'})
+
+    available_formats = {}
+    for fmt_id in config:gmatch("iplayer_streaming_[%w_]+") do
+        available_formats[fmt_id] = true
+    end
+    -- Create the list of acceptable formats, ordered by preference
+    local r = self.requested_format
+    local preferred = ((r == 'best') and {'high', 'standard', 'low', 'vlow'})
+                   or ((r == 'default') and {'standard', 'low', 'vlow', 'high'})
+                   or {r} 
+
+    -- Pick the first acceptable format available
+    local format
+    for _, cur_format in ipairs(preferred) do
+        if available_formats[fmt_id_lookup[cur_format]] then
+            format = cur_format
+            break
+        end
+    end
+    if not format then error('format not available') end
+
+    -- Iterate over <media/>s
+    local media
+    for section in config:gmatch('<media .-</media>') do
+        if section:find('service="' .. fmt_id_lookup[format] .. '"') then
+            media = section
+            break
+        end
+    end
+    if not media then error('couldn\'t parse the config') end
+
+    self.url = {}
+
+    for connection in media:gmatch('<connection .-/>') do
+        local params, complete_uri = {}, ''
+        for _,param in pairs{'supplier', 'server', 'application', 'identifier', 'authString'} do
+            _,_,params[param] = connection:find(param .. '="(.-)"')
+        end
+        
+        -- in 'application', mp has a value containing one or more entries separated by strings.
+        -- We only keep the first entry.
+        params.application = params.application:gsub("&mp=([^,&]+),?.-&", "&mp=%1&"))
+
+        if params.supplier == 'akamai' then
+            process_akamai(params)
+        end
+
+        if (params.supplier == 'limelight' or params.supplier == 'level3') then
+            process_limelight_level3(params)
+        end
+
+        params.application = params.application or 'Please report'
+        params.uri = params.uri or 'Please report'
+
+        complete_uri = params.uri
+            .. ' app=' .. params.application
+            .. ' playpath=' .. params.identifier
+            .. ' swfUrl=http://www.bbc.co.uk/emp/revisions/18269_21576_10player.swf?revision=18269_21576 swfVfy=1'
+        if params.tcurl then
+            complete_uri = complete_uri .. ' tcUrl=' .. params.tcurl
+        end
+        self.url[#(self.url) + 1] = complete_uri
+    end
+
+    return self
+end
